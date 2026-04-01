5b43c2ecb4adafbce6a9cceb4f7385e37fcb4d76
kori756 <khorirachmatr@gmail.com>
2026-04-01 13:27:57 +0700

    menyelesaikan function untuk bilangan fibonachi

--- tugaslopping.cpp
+++ tugaslopping.cpp
@@ -20,4 +20,14 @@ bool isPrima(int n) {
 bool isFibonacci(int n){
      int a = 0, b = 1, c = 0;
 
-    
+     while (c <= n) {
+        if (c == n) {
+            return true;
+
+             }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return false;
+}
