e3eef63e2a73397033c3d0dd65402d51279eb8aa
kori756 <khorirachmatr@gmail.com>
2026-04-01 13:24:55 +0700

    menambahkan function untuk bilangan prima

--- tugaslopping.cpp
+++ tugaslopping.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int angka;
 int pilihan;
+
+bool isPrima(int n) {
+    if (n <= 1) return false;
+
+    
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+            }
+         }
+    return true; 
+}
